ad139e0e0d7054a3842fa4637d43a07f49f23524
Pandaa <pandaachu@gmail.com>
2024-11-24 14:36:20 +0800

    fixed: depoly fail problem

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/libs/auth';
 import prisma from '@/libs/prismadb';
 
 export async function getSession() {
